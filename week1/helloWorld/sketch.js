9bac62648283543342f1ab1caa2ca5a18da2a473
oprior <104953856+oprior@users.noreply.github.com>
2022-05-05 16:05:46 -0400

    Add files via upload

---
+++ week1/helloWorld/sketch.js
@@ -0,0 +1,14 @@
+
+/** Displays the text "Hello World" in the center of the window **/
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+  text('Hello world!', windowWidth/2, windowHeight/2);
+
+}
